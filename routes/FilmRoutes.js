52b538d438001abeb07de9af8a4a0945aaba6771
Bitzz121 <abitsolihin8@gmail.com>
2022-10-19 20:19:16 +0700

    change route

--- routes/FilmRoutes.js
+++ routes/FilmRoutes.js
@@ -3,12 +3,12 @@ import { getFilms, getFilmById, saveFilm, updateFilm, deleteFilm, getFilmByGenre
 
 const router = express.Router();
 
-router.get('/api/film', getFilms)
-router.get('/api/film/genre/:genre', getFilmByGenre)
-router.get('/api/film/:id', getFilmById)
-router.get('/api/film/:endpoint', getFilmByEndpoint)
-router.post('/api/film/', saveFilm);
-router.patch('/api/film/:id', updateFilm);
-router.delete('/api/film/:id', deleteFilm);
+router.get('/api/films', getFilms)
+router.get('/api/films/genre/:genre', getFilmByGenre)
+router.get('/api/films/:id', getFilmById)
+router.get('/api/films/film/:endpoint', getFilmByEndpoint)
+router.post('/api/films/', saveFilm);
+router.patch('/api/films/:id', updateFilm);
+router.delete('/api/films/:id', deleteFilm);
 
 export default router;
