0f672d9e0e5b6e9002e5ed4b360b1caf4410f309
Bitzz121 <abitsolihin8@gmail.com>
2022-10-25 16:04:06 +0700

    change database

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('railway', 'root', '7Br5fUh2ODeSvrU9zyrO', {
-  host: 'containers-us-west-32.railway.app',
+const db = new Sequelize('railway', 'root', '0dbgwrf1Brt2rYPYL7xB', {
+  host: 'containers-us-west-70.railway.app',
   dialect: 'mysql',
-  port: 5821,
+  port: 7433,
 });
 // const db = new Sequelize('web_film', 'root', '', {
 //   host: 'localhost',
