9bfabd183e6eeacc532e687da3d2054238004c15
Bitzz121 <abitsolihin8@gmail.com>
2022-10-26 20:49:24 +0700

    fix query

--- controllers/FilmController.js
+++ controllers/FilmController.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export const getFilms = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
-  const limit = 10;
+  const limit = parseInt(req.query.limit) || 12;
   const offset = (page > 1) ? (page * limit) - limit : 0;
   try {
     const response = await Film.findAll({limit,offset});
