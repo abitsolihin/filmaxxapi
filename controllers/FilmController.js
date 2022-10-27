38c4e65d0203ae136bc43bf8d8563f1b208890b3
Bitzz121 <abitsolihin8@gmail.com>
2022-10-27 17:13:52 +0700

    fix

--- controllers/FilmController.js
+++ controllers/FilmController.js
@@ -9,6 +9,7 @@ export const getFilms = async (req, res) => {
   try {
     const response = await Film.findAndCountAll({limit,offset});
     res.json(response);
+    response.rows;
   } catch (error) {
     console.log(error.message);
   }
