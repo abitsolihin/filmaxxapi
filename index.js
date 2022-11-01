43f5ff7f9cc795a30c7976ad05a7416f8b13695d
Bitzz121 <abitsolihin8@gmail.com>
2022-11-01 13:33:32 +0700

    fix

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000"
+    // origin: "http://localhost:3000"
+    origin: "https://filmaxx.vercel.app"
 }));
 app.use(express.static('public'));
 app.use(fileUpload());
