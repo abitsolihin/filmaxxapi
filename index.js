42453c62be35a252a5d47399a70ed302590021a1
Bitzz121 <abitsolihin8@gmail.com>
2022-11-01 13:28:14 +0700

    add cors

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import FilmRoute from './routes/FilmRoutes.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 app.use(express.static('public'));
 app.use(fileUpload());
 app.use(FilmRoute);
